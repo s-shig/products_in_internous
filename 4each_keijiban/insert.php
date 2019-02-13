7dbd7adecb7ea6143006f55761fa664b36d0b04d
shinichi shigenai <s-shig@shinichinoMBP.mshome.net>
2019-02-13 09:31:18 +0900

    PHPで掲示板を作成

---
+++ 4each_keijiban/insert.php
@@ -0,0 +1,9 @@
+<?php
+mb_internal_encoding("utf8");
+$pdo = new PDO("mysql:dbname=lesson01;host=localhost;" ,"root" ,"mysql");
+
+$pdo->exec("insert into 4each_keijiban(handlename,title,comments)
+values('".$_POST['handlename']."','".$_POST['title']."','".$_POST['comments']."');");
+header("Location:./index.php");
+
+?>
